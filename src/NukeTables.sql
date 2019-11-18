6f5e11c3ad07c5fbbd9522baaeba8fbf83614a68
Will Coughlin <willcoughlin@gmail.com>
2019-11-18 16:39:42 -0600

    Fix order of DROP TABLES in script

--- src/NukeTables.sql
+++ src/NukeTables.sql
@@ -1,8 +1,8 @@
 drop table `Book`;
+drop table `OrderDetails`;
+drop table `Order`;
 drop table `Customer`;
 drop table `Employee`;
-drop table `Order`;
-drop table `OrderDetails`;
 drop table `Shipper`;
 drop table `Subject`;
 drop table `Supplier`;
